945fab4b6816d9a3608f6cbe9db35aab21798cf8
Zuhair <zuhair2945@gmail.com>
2025-11-23 22:56:32 +0530

    cleaned projects

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -68,16 +68,7 @@ export default function ProjectsSection() {
             live: "https://wanderlust-j3s6.onrender.com/",
             code: "https://github.com/Zuhair-CS/Wanderlust",
             src: "/images/wanderlust.png",
-        },
-        {
-            name: "AI WEB SCRAPER",
-            type: "Apify + Next.js integration",
-            description: "Runs a custom Apify actor to scrape a page and summarize H1—H6 headings. Advanced web scraping and analysis tool with modern UI.",
-            tech: ["Apify", "Next.js", "Next.js API routes"],
-            live: "https://apify-implementation.vercel.app/",
-            code: "https://github.com/Zuhair-CS/Apify_Implementation",
-            src: "/images/apify.png",
-        },
+        }
     ];
     return (_jsx("section", { id: "projects", className: "py-10", children: _jsxs("div", { className: "max-w-5xl mx-auto px-6", children: [_jsx(AnimatedContainer, { children: _jsxs("div", { className: "flex items-center mb-20", children: [_jsx("div", { className: "flex-1 h-[2px] bg-gradient-to-r from-blue-500 to-purple-500" }), _jsx("h2", { className: "text-5xl font-bold text-white ml-4", children: "Projects." })] }) }), _jsx(ExpandableCardDemo, { projects: projects })] }) }));
 }
